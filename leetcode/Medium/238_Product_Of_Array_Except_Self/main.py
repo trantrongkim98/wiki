dafaa99df569c641028d3f97624d5124b0913572
kimtt <trantrongkim98@gmail.com>
2026-06-09 23:50:34 +0700

    feat(238): done

--- leetcode/Medium/238_Product_Of_Array_Except_Self/main.py
+++ leetcode/Medium/238_Product_Of_Array_Except_Self/main.py
@@ -19,6 +19,14 @@ class Solution:
             iAns+=1
         return answers
         
+# Sử dụng 2 con trỏ để giải quyết bài toán này
+# Chia mảng thành 2 phần trái và phải
+# Sau đó tính tích bên phải và bên trái
+# Tích bên trái sẽ bằng tích của các phần tử đằng trước nó 
+# suy ra ta có index bắt đầu từ 1 và công thức arrLeft[left-1]*nums[left-1]
+# Tích bên phải sẽ bằng tích của các phần tử đằng sau nó
+# suy ra ta có index bắt đầu từ lengh -2 và công thức arrRight[right+1] * nums[right+1] 
+# Cuối cùng nhân 2 mảng trái phải lại với nhau sẽ được kết quả
     
 s = Solution()
 nums = [1,2,3,4]
